33d64a4b477797a8c41925590e9ca67564566468
Zainul884 <zainul.malik@edu.sait.ca>
2023-11-09 00:17:47 -0700

    Initial commit

---
+++ ToDoForm.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { View, TextInput, Button, StyleSheet } from 'react-native';
+
+const ToDoForm = () => {
+  return (
+    <View style={styles.form}>
+      <TextInput
+        style={styles.input}
+        placeholder="Add a new task..."
+      />
+      <Button title="Add" />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+});
+
+export default ToDoForm;
---
+++ ToDoList.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { ScrollView, Pressable, View, Text, StyleSheet } from 'react-native';
+
+const ToDoList = () => {
+  return (
+    <ScrollView>
+      <Pressable>
+        <View style={[styles.task]}>
+          <Text style={styles.taskText}>Do laundry</Text>
+        </View>
+      </Pressable>
+      <Pressable>
+        <View style={[styles.task]}>
+          <Text style={styles.taskText}>Go to gym</Text>
+        </View>
+      </Pressable>
+      {}
+    </ScrollView>
+  );
+};
+
+const styles = StyleSheet.create({
+    task: {
+        padding: 10,
+        borderBottomWidth: 1,
+        borderColor: '#ccc',
+    },
+    taskText: {
+        fontSize: 16,
+    },
+});
+
+export default ToDoList;
